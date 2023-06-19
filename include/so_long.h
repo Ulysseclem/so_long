97e2b54e9873cc263557439b299b3fd2701d3561
Ulysse CLEMENT <uclement@bess-f1r3s8.clusters.42paris.fr>
2023-06-19 15:14:49 +0200

    test 2

--- include/so_long.h
+++ include/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ulysse <ulysse@student.42.fr>              +#+  +:+       +#+        */
+/*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 11:20:00 by ulysse            #+#    #+#             */
-/*   Updated: 2023/06/18 12:00:35 by ulysse           ###   ########.fr       */
+/*   Updated: 2023/06/19 15:12:34 by uclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,46 @@
 # define SO_LONG_H
 
 # include "mlx.h"
+# include <stdio.h>
+# include <X11/keysym.h>
+# include <stdlib.h>
+# include <X11/X.h>
 
-typedef struct	s_data {
-	void	*img;
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
+
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+#define GREEN_PIXEL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_img
+{
+	void	*mlx_img;
 	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
+	int		bpp; /* bits per pixel */
+	int		line_len;
 	int		endian;
-}				t_data;
+}	t_img;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+	int		cur_img;
+}	t_data;
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
 
 int main(void);
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 #endif
